b015291cf83664133857a4c830a946d6ca67f03d
Young Min <dudals3844@gmail.com>
2019-09-12 06:51:03 +0000

    normal type

--- index.php
+++ index.php
@@ -5,92 +5,84 @@
 <body>
 	<h1>Hello goorm</h1>
 	<?php
-		echo "Contents here wow";
-		$var = 10;
-		echo "$var";
-		echo"{$var}";//var값 표시
-		echo $bool ?"true<br>": "false<br>";//기본 불리언 값
-		//php연산자
-		var_dump($var);//NULL값
-		$int += 15; var_dump($int);//기본값에다 15 더해서 15임
-		$flt += 3.14; var_dump($flt);
-		$str .= "programming";var_dump($str);
-		$arr[2] = "second array"; var_dump($arr);
-		//local variable
-		/*
-		function varFunc(){
-			$var = 10;
-			echo "inside variable var is {$var}.<br>";
-		}
-		varFunc();
-		echo "outside variable var is {$var}";
-		*/
-		//global variable
-		$var = 10;
-		function varFunc(){
-			echo "inside function global variable is {$var}";//출력값 없음
-			global $var;
-			echo "inside function global variable is {$var}";//global로 변수 호출해줘야지 출력
-			echo "inside function GLOBALS variable is {$GLOBALS['var']}";// 
-			
-			
-			
-		}
-		varFunc();
-		echo "outside variable var is {$var}";
+	//normal type
+	var_dump((bool) false);//false
+	var_dump((bool) "false");//true
+	var_dump((bool) 0);//false
+	var_dump((bool) -100);//true
+	var_dump((bool) 0.0);//false
+	var_dump((bool) "");//false
+	var_dump((bool) "0");//false
+	var_dump((bool) array());//false
+	var_dump((bool) null);//false
 	
-		//static variable
-		function counter(){
-			static $count = 0;
-			echo " call inside function static variable count is {$count}";
-			$count++;
-		}
+	//integer
+	echo "integer type size is ".PHP_INT_SIZE." <br> ";
+	echo "integer max type size is ".PHP_INT_MAX." <br> ";
+	$int_01 = 100;
+	$int_02 = 2147483647; // integer가 표현할 수 있는 범위를 넘지 않는 값을 대입함.
+	$int_03 = 2147483648; // integer가 표현할 수 있는 범위를 넘는 값을 대입함. 이 변수는 자동으로 float됌
 	
-		counter();
-		counter();
-		counter();
-		//define(상수이름,상숫값,대소문자 구분 여부)
-		/*
-		define("PHP","Welcome to PHP class<br>");//대소문자를 구분함 (기본설정)
-		echo PHP;
-		echo php;
-		define("PHP","Welcome to PHP class<br>",true);//대소문자를 구분안함
-		echo PHP;//구분 안함
-		echo php;//구분 안함
-		
-		function defFunc(){
-			echo PHP;
-			define("PHP","Welcom to PHP class");
-			echo PHP;
-		}
+	//float
+	$float_01 = 3.14;
+	$float_02 = 1.23e-4; // float이 표현할 수 있는 범위를 넘지 않는 값을 대입함.
+	$float_03 = 1.8E308; // float이 표현할 수 있는 범위를 넘는 값을 대입함.
+	//위에 값은 자동으로 INF 즉 무한대로 간다.
 	
-		defFunc();
-		echo PHP;
-		*/
-		//magic constants
-		//echo "<pre>";
-		//print_r(get_defined_constants(true));
-		//echo "</pre>"
-		/*
-		__LINE__	파일의 현재 줄 번호를 반환함.
-		__FILE__	파일의 전체 경로와 이름을 반환함. include 내부에서 사용할 경우 include된 파일명을 반환함.
-		__DIR__		파일의 디렉터리를 반환함.포함한 파일 안에서 사용할 경우 포함된 파일의 디렉터리를 반환함.
-					dirname(__FILE__)과 같은 결과를 반환함.
-
-		__FUNCTION__	함수의 이름을 반환함.
-		__CLASS__	클래스의 이름을 반환함. 클래스 이름은 대소문자를 구분함.
-		__TRAIT__	트레이트(trait)의 이름을 반환함. 트레이트의 이름은 트레이트를 선언한 네임스페이스를 포함함.
-
-		__METHOD__	클래스의 메소드 이름을 반환함.
-		__NAMESPACE__	현재 네임스페이스의 이름을 반환함.
-		
-		function magicCons() {
-   			echo __LINE__;     // 파일의 현재 줄 번호를 반환함.
-    		echo __FUNCTION__; // 함수의 이름을 반환함.
-    		echo __METHOD__;   // 클래스의 메소드 이름을 반환함.
+	//string
+	$str_01 = "PHP";
+	$str_02 = "JAVAscript";
+	echo strlen($str_01);//3
+	echo strlen($str_02);//10
+	
+	//array는 key와 value로 이루어져 있다
+	
+	$arr = array(
+		1 => " first value", //1과 "1"은 같은 값임
+		"1" => " second value",//같은 키값으로 두번 선언해서 결국 두번쨰 키값만 남음
+		10 => " third value",
+		-10 => " forth value"
+	);
+	
+	var_dump($arr);
+	echo $arr[1];
+	echo $arr["1"];
+	echo $arr[10];
+	echo $arr[-10];
+	
+	//object
+	
+	class Lecture {
+		function Lecture(){
+			$this->lec_01 = "PHP";
+			$this->lec_02 = "MYSQL";
+			
 		}
-		magicCons();
-		*/
+	}
+	
+	$var = new Lecture;//객체 생성
+	echo $var->lec_01;
+	echo "<br>";
+	echo $var->lec_02;
+	
+	//NULL
+	
+	$var_01;
+	var_dump($var_01);
+	
+	$var_01 = 100;//변수 초기화
+	var_dump($var_01);//초기화해서 제대로 나옴
+	
+	unset($var_02);//var_02를 삭제
+	var_dump($var_02);//삭제되는 변수를 참조
+	
+	
+	
+	
+	
+	
+	
+	
 	?>
 </body>
 </html>
