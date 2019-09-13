f50e35b7842074f48c15e0de4f564bee644768d8
Young Min <dudals3844@gmail.com>
2019-09-13 13:46:17 +0000

    multidimensional array

--- hello.php
+++ hello.php
@@ -1,30 +1,30 @@
 <?php
-    //array
-    $arr = array();//배열 생성
-    $arr[0] = "apple";
-    $arr[1] = "banana";
-    $arr[2] = "orange";
+    //multidimensional array
+
+    //2차원 배열
+    $arr = array(
+        array(),
+        array(),
+        array()
+    );
+
+    /*
+    $arr = array(
+        array("apple", "korea", "1000"),
+        array("banana", "philoppines",2000s),
+        array()
+    );
+
+    */
+    $arr[0][0] = "apple";
+    $arr[0][1] = "korea";
+    $arr[0][2] = "1000";
+
+    $arr[1][0] = "banana";
+    $arr[1][1] = "philippines";
+    $arr[1][2] = 2000;
 
 
-    $arr2 = array("choi", "young", "min");
-
-    $arr3 = array();
-    $arr3[10] = "banana";
-
-    var_dump($arr3);
-    var_dump($arr3[0]);
-    var_dump(isset($arr3[0]));
-    var_dump(isset($arr3[10]));
-    //isset은 변수가 초기화 되어있는지 확인
-
-    $arr4 = array("apple", "banana", "orange");
-    for($i = 0; $i < count($arr4);$i++){
-        echo $arr[$i]."<br>";
-    }
-
-    foreach($arr4 as $element){
-        echo $element;
-    }
-
-
+    echo $arr[0][0].", ".$arr[0][1].", ".$arr[0][2]."<br>";
+    echo $arr[1][0].", ".$arr[1][1].", ".$arr[1][2]."<br>";
 ?>
