6133bc0bb2f16f579ff307a29a79ed25cc8c41cc
Young Min <dudals3844@gmail.com>
2019-09-13 13:18:53 +0000

    while do-while

--- hello.php
+++ hello.php
@@ -1,37 +1,15 @@
 <?php
-//if
-$num_01 = 10;
-$num_02 = 11;
+//while do.while
 
-    if($num_01 == $num_02){
-        echo "{$num_01} same {$num_02}";
-    }
-    else if($num_01 < $num_02){
-        echo "{$num_01} < {$num_02}";
-    }
-    else if($num_01 > $num_02){
-        echo "{$num_01} > {$num_02}";
+    $i = 0;
+    while ($i < 5) {
+        echo($i++)."<br>";
     }
 
+    $j = 0;
 
-    $var = "귤";
-    switch ($var) {
-        case "귤":
-            echo "this is korea orange";
-            break;
-        case "사과":
-        case "바나나":
-        case "아보카도":
-            echo "여기 있는 과일은 제가 먹어본 과일입니다.";
-            break;
-        case "파파야":
-        case "두리안":
-        case "석가":
-            echo "여기 있는 과일은 제가 먹어보지 못한 과일입니다.";
-            break;
-        default:
-            echo "여기 있는 것은 과일이 아닙니다.";
-            break;
-}
-
+    do {
+        echo "variable j is ".(++$j)." ";
+    }while($j > 5);
+    //한번은 실행됨
 ?>
