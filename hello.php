e1fa3a118fbc3e3169205915ab041b15b1160093
Young Min <dudals3844@gmail.com>
2019-09-13 13:38:08 +0000

    array

--- hello.php
+++ hello.php
@@ -1,24 +1,30 @@
 <?php
-    //continue break
-    $excepNum = 4;
-    for ($i = 0; $i < 100; $i++){
-        if($i = 0 % $excepNum == 0){
-            continue;
-        }
-        echo "{$i} ";
+    //array
+    $arr = array();//배열 생성
+    $arr[0] = "apple";
+    $arr[1] = "banana";
+    $arr[2] = "orange";
+
+
+    $arr2 = array("choi", "young", "min");
+
+    $arr3 = array();
+    $arr3[10] = "banana";
+
+    var_dump($arr3);
+    var_dump($arr3[0]);
+    var_dump(isset($arr3[0]));
+    var_dump(isset($arr3[10]));
+    //isset은 변수가 초기화 되어있는지 확인
+
+    $arr4 = array("apple", "banana", "orange");
+    for($i = 0; $i < count($arr4);$i++){
+        echo $arr[$i]."<br>";
     }
 
-    $sum = 0;
-    $startNum = 1;
-    $endNum = 100;
-    $i = $startNum;
-    while(true){
-        $sum += $i;
-        if($i == $endNum){
-            break;
-        }
-        $i++;
+    foreach($arr4 as $element){
+        echo $element;
     }
 
-    echo "{$startNum}  {$endNum}  {$sum}" ;
+
 ?>
