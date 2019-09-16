2802f177f3b04385fad6274deebdbbf97fe165ec
Young Min <dudals3844@gmail.com>
2019-09-16 10:47:59 +0000

    Inheritance

--- hello.php
+++ hello.php
@@ -1,69 +1,28 @@
 <?php
     //http://tcpschool.com/php/php_function_basic 공부하는 사이트
-    //Using class
+    //Inheritance
 
-    class ClassName {
-        public $publicVar;
-        private $privateVar;
-        protected $protectedVar;
+    //Overriding
 
-
-        public function _construtor(){
-            $this->plublicVar = "public property ";
-            $this->privateVar = "private property";
-            $this->protectedVar = "protected property";
-
-        }
-
-
-        public function publicMethod(){
-            echo "public method ";
-        }
-
-        protected function protectedMethod(){
-            echo "protected method";
-        }
-
-        private function privateMethod(){
-            echo "private method ";
+    class A {
+        public $property = "class A";
+        public function showProperty(){
+            echo $this->property;
         }
     }
 
+    class B extends A {
+        public $property = "class B";
 
-
-    $object = new ClassName();
-
-    echo $object ->plublicVar;
-    //echo $object ->protectedVar;//접근 안됌
-    //echo $object ->privateVar;//접근 안됌
-
-    $object->publicMethod();      // 호출 가능
-    //$object->protectedMethod(); // 호출 불가능
-    //$object->privateMethod();   // 호출 불가능
-
-
-    //data hiding
-    class ClassName2 {
-        private $privateVar;
-
-        public function _constructor(){
-            $this ->privateVar = "private property";
-        }
-
-        public function getValue(){
-            return $this ->privateVar;
-        }
-
-        public function setValue($value){
-            $this ->privateVar = $value;
+        //overriding
+        public function showProperty(){
+            echo "hello".$this->property."<br>";
         }
     }
 
-
-
-    $object2 = new ClassName2();
-
-    $object2->setValue("hello");
-    echo $object2 ->getValue();
+    $a = new A();
+    $a ->showProperty();// call class A method
+    $b = new B();
+    $b ->showProperty();//call class B method
 
 ?>
