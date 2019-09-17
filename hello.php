634769e652e66015d938f26e041dc86651b13dc8
Young Min <dudals3844@gmail.com>
2019-09-17 11:07:30 +0000

    Late static bindings

--- hello.php
+++ hello.php
@@ -1,72 +1,56 @@
 <?php
     //http://tcpschool.com/php/php_function_basic 공부하는 사이트
-    //Interface
-
-    //Overloading
-
-
-    //Property overloading
-    class PropertyOverloading {
-        private $data = array();//오버로딩 변수 저장
-        public $declared = 10;
-        private $hidden = 20;
-
-        public function __set($name, $value){
-        echo "$name property {$name}";
-        $this ->data[$name] = $value;
+    //Late static bindings
+    /*
+    class A {
+        public static function className(){
+            echo __CLASS__;
         }
 
-        public function __get($name){
-            echo "$name";
-            if(array_key_exists($name,$this->$data)){
-                return $this ->data[$name];
-            } else {
-                return null;
-            }
-        }
-
-        public function __isset($name){
-            echo "$name 프로퍼티가 설정되어 있는지 확인";
-            return isset($this->data[$name]);
-        }
-
-        public function __unset($name){
-            echo "$name 프로퍼티 해제";
-            unset($this ->data[$name]);
+        public static function printClass(){
+            //self::className();//정적 메소드
+            static::className();//늦은 정적 메소드
         }
     }
 
-    $obj = new PropertyOverloading();
 
-    $obj->prop = 1;// 동적 프로퍼티 생성
-    echo $obj->prop; // 동적 프로퍼티에 접근
-    //var_dump(isset($obj->prop));동적 프로퍼티로 isset() 함수 호출
-    unset($obj->prop);//동적 프로퍼티로 unset() 함수 호출
-    var_dump(isset($obj->prop));
-
-    echo $obj->declared;
-
-    //Method Overloading
-
-    class MethodOverloading{
-        public function __call($name, $arguments){
-            echo join(",",$arguments);
-        }
-
-        public static function __callStatic($name, $arguments){
-            echo join(",",$arguments);
+    class B extends A {
+        public static function className(){
+            echo __CLASS__;
         }
     }
 
-    $obj2 = new MethodOverloading();
+    B::printClass();
+    */
 
-    $obj2 ->testMethod("Class area");
+    //call non static method late static bindings
 
-    MethodOverloading::testMethod;
+    class A {
+        private function className(){
+            echo __CLASS__;
+        }
 
+        public function printClass(){
+            $this ->className();
+            static::className();
+        }
+    }
 
+    class B extends A {
 
+    }
 
+    class C extends A {
+        //overriding
+        private function className(){
 
+        }
+    }
+
+    $b = new B();
+    $b -> printClass();
+
+    $c = new C();
+    //$c -> printClass();
 
 ?>
