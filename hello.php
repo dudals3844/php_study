3766df3afd4e299fbfa31fbaa21c197f3d2dbb8e
Young Min <dudals3844@gmail.com>
2019-09-17 09:57:27 +0000

    Interface

--- hello.php
+++ hello.php
@@ -1,21 +1,53 @@
 <?php
     //http://tcpschool.com/php/php_function_basic 공부하는 사이트
-    //Static Members
+    //Interface
 
-    // 단순히 클래스의 정의 내에서 프로퍼티나 메소드를 사용하고 싶을 때는 범위 지정 연산자(::)를 사용할 수 있습니다.
+    //abstract class
+    abstract class AbstractClass {
+        abstract protected function move();//추상메소드
+        abstract protected function stop();
 
-    class StaticMember {
-        public static $staticProperty = "static property";
-        public static function showProperty(){
-            echo self::$staticProperty;//self: 자기 자신에 접근
-            //parent: 부모 클래스에 접근할때
+        public function start(){
+
         }
     }
 
-    echo StaticMember::showProperty();//호출가능
-    echo StaticMember::$staticProperty;//접근 가능
 
-    $var = new StaticMember();//create instance
-    echo $var->showProperty();
-    //echo $var ->staticProperty;//접근 불가
-?>
+    //interface
+
+    interface Transport{
+        public function move();
+        public function stop();
+    }
+    /*
+
+    class Car implements Transport {//Transport를 구현하는 Car클래스
+        function move(){
+
+        }
+
+        function stop(){
+
+        }
+
+    }
+    */
+
+    interface Overland extends Transport {
+        public function highpass();//구현할 메소드
+    }
+
+
+    class Car implements Overland {
+        function move(){
+
+        }
+
+        function stop(){
+
+        }
+
+        function highpass(){
+
+        }
+    }
