bea96c6358176a7b9c9a5c13c293bd312255df79
Young Min <dudals3844@gmail.com>
2019-09-17 13:33:39 +0000

    Form example

--- php_handling_request_01.php
+++ php_handling_request_01.php
@@ -1,7 +1,22 @@
 <?php
     $name = $_POST["name"];//html에서 name이란 id에서 보낸 값을 받아온다.
+    $gender = $_POST["gender"];
     $email = $_POST["email"];
+    $website = $_POST["website"];
+    $favtopic = $_POST["favtopic"];
+    $comment = $_POST["comment"];
 
-    echo $name."님의 이메일 주소는 ".$email."입니다.";
+    echo "<h2>입력된 회원 정보</h2>";
+    echo "이름 : ".$name."<br>";
+    echo "성별 : ".$gender."<br>";
+    echo "이메일 : ".$email."<br>";
+    echo "홈페이지 : ".$website."<br>";
+    echo "관심 있는 분야 : ";
+    if (!empty($favtopic)) {
+        foreach ($favtopic as $value) {
+            echo $value." ";
+        }
+    }
+    echo "<br>기타 : ".$comment;
 
 ?>
