f7f516108e3a913f15913399cb71e3d681eaef27
Young Min <dudals3844@gmail.com>
2019-09-17 14:21:29 +0000

    Cookies

--- php_handling_request_01.php
+++ php_handling_request_01.php
@@ -1,10 +1,19 @@
 <?php
-    $name = $_POST["name"];//html에서 name이란 id에서 보낸 값을 받아온다.
+    //$name = $_POST["name"];//html에서 name이란 id에서 보낸 값을 받아온다.
+    if(empty($_POST["name"])){
+        echo "Input the name";
+        echo "<meta http-equiv='refresh' content='0' url='/index.html'>";
+
+    } else {
+        $name = $_POST["name"];
+    }
     $gender = $_POST["gender"];
     $email = $_POST["email"];
     $website = $_POST["website"];
     $favtopic = $_POST["favtopic"];
     $comment = $_POST["comment"];
+
+
 
     echo "<h2>입력된 회원 정보</h2>";
     echo "이름 : ".$name."<br>";
