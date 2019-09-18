cd4fc5b4cdae3cb4b88b689a516e1fcaa09eb544
Young Min <dudals3844@gmail.com>
2019-09-18 09:50:31 +0000

    User defined Error handling

--- index.php
+++ index.php
@@ -1,30 +1,49 @@
-<!DOCTYPE html>
-
-<html lang="ko">
-
-<head>
-	<meta charset="UTF-8">
-	<title>PHP Cookie And Session</title>
-</head>
-
-<body>
-
-
-
 <?php
-	//exception handling
-	try{
-		throw new Exception("exception message");
+	//user defined exception
+
+	class CustomException extends Exception {
+		public function errorMessage(){
+			$msg = $this->getMessage()."<br/>".
+			"error file address: ".$this->getFile()."<br/>".
+			"error line number: ".$this->getLine();
+
+			return $msg;
+		}
+	}
+
+	try {
+		throw new CustomException("error message");
+
+	} catch (CustomException $ex){
+		echo $ex->errorMessage();
+	}
+
+	//중첩 try
+	try {
+		try{
+			throw new CustomException("error message");
+		}
+		catch(CustomException $ex){
+			throw $ex;
+		}
+	}
+
+	catch (Exception $ex){
+		echo $ex->getMessage();
+	}
+
+	//다중 catch
+	try {
+		throw new CustomException("error message");
+	}
+	catch(customException $ex){
+		echo $ex->errorMessage();
 	}
 	catch (Exception $ex){
-		echo $ex->getMessage()."<br>";
-		echo "file address: ".$ex->getFile()."<br>";
-		echo "error line: ".$ex->getLine();
+		echo $ex->getMessage();
 	}
+
+
+
 ?>
-
-
-</body>
-
-</html>
 
